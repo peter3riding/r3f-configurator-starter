ad0a365683a70978124dfabd308bccba5fbc165d
peterridingdev <peterdriding@gmail.com>
2026-07-07 15:27:56 +0400

    feat: add url-state

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -11,6 +11,7 @@ type State = {
   intro: boolean;
   selectedColorId: ColorKey;
   selectedDecalId: DecalKey;
+  cart: [];
 
   downloadHandler: (() => void) | null;
 };
@@ -21,6 +22,9 @@ type Actions = {
   selectDecal: (id: DecalKey) => void;
   registerDownload: (fn: (() => void) | null) => void;
   download: () => void;
+  hydrateFromURL: () => void;
+  syncURL: () => void;
+  addToCart: () => void;
 };
 
 const useStore = create<State & Actions>()(
@@ -30,17 +34,59 @@ const useStore = create<State & Actions>()(
       selectedColorId: "gold",
       selectedDecalId: "three2",
 
+      cart: [],
+
       downloadHandler: null,
 
       setIntro: (value) => set({ intro: value }, false, "setIntro"),
-      selectColor: (id) => set({ selectedColorId: id }, false, "selectColor"),
-      selectDecal: (id) => set({ selectedDecalId: id }, false, "selectDecal"),
+      selectColor: (id) => {
+        set({ selectedColorId: id }, false, "selectColor");
+        get().syncURL();
+      },
+      selectDecal: (id) => {
+        set({ selectedDecalId: id }, false, "selectDecal");
+        get().syncURL();
+      },
+
+      addToCart: () => {
+        const cartTest = get().cart;
+        const updatedCart = [...cartTest, useTotalPrice()];
+      },
 
       registerDownload: (fn) =>
         set({ downloadHandler: fn }, false, "registerDownload"),
 
       download: () => {
         get().downloadHandler?.();
+      },
+
+      hydrateFromURL: () => {
+        const params = new URLSearchParams(window.location.search);
+        const color = params.get("color");
+        const decal = params.get("decal");
+
+        set(
+          {
+            ...(color &&
+              color in shirtConfig.colors && {
+                selectedColorId: color as ColorKey,
+              }),
+            ...(decal &&
+              decal in shirtConfig.decals && {
+                selectedDecalId: decal as DecalKey,
+              }),
+          },
+          false,
+          "hydrateFromURL",
+        );
+      },
+      syncURL: () => {
+        const { selectedColorId, selectedDecalId } = get();
+        const params = new URLSearchParams({
+          color: selectedColorId,
+          decal: selectedDecalId,
+        });
+        history.replaceState(null, "", `?${params}`);
       },
     }),
     { name: "Shirt Configurator" },
